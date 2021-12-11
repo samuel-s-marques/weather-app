605846b4a1b79f4512f50c705fae96176523220a
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-12-11 18:34:08 -0300

    Add appbar and forecast button

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -24,22 +24,66 @@ class _HomePageState extends State<HomePage> {
       ),
       child: Scaffold(
         backgroundColor: Colors.transparent,
-        appBar: AppBar(),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          toolbarHeight: 120,
+          title: Padding(
+            padding: const EdgeInsets.only(left: 22),
+            child: TextButton(
+              onPressed: () {},
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "São Paulo",
+                    style: GoogleFonts.getFont("Overpass",
+                        fontWeight: FontWeight.bold,
+                        fontSize: 22,
+                        color: Colors.white),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 15.0),
+                    child: Icon(Icons.expand_more, color: Colors.white),
+                  )
+                ],
+              ),
+            ),
+          ),
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.place_outlined),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 10.0),
+              child: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.notifications_none),
+              ),
+            )
+          ],
+        ),
         body: Column(
           children: [
-            const Center(
-              child: BoxedIcon(
-                WeatherIcons.day_sunny,
-                size: 170,
-                color: Colors.white,
+            Padding(
+              padding: const EdgeInsets.only(top: 10, bottom: 28),
+              child: Center(
+                child: Image.asset(
+                  "assets/images/light-mode/drop-light.png",
+                  width: 171,
+                )
               ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 30),
               child: Card(
-                color: Colors.transparent,
+                color: Colors.white.withOpacity(0.2),
+                elevation: 0,
                 shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
+                  side: BorderSide(color: Colors.white, width: 1.85),
+                  borderRadius: BorderRadius.circular(20),
+                ),
                 child: Padding(
                   padding: const EdgeInsets.only(bottom: 26, top: 17),
                   child: Column(
@@ -50,10 +94,13 @@ class _HomePageState extends State<HomePage> {
                         style: GoogleFonts.getFont("Overpass",
                             fontSize: 18, color: Colors.white),
                       ),
-                      Text(
-                        "29°",
-                        style: GoogleFonts.getFont("Overpass",
-                            fontSize: 100, color: Colors.white),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 12.0),
+                        child: Text(
+                          "29°",
+                          style: GoogleFonts.getFont("Overpass",
+                              fontSize: 100, color: Colors.white),
+                        ),
                       ),
                       Padding(
                         padding: const EdgeInsets.only(bottom: 30),
@@ -72,28 +119,27 @@ class _HomePageState extends State<HomePage> {
                         children: [
                           Column(
                             children: [
-                              Text(
-                                "Wind",
-                                style: GoogleFonts.getFont(
-                                  "Overpass",
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                ),
+                              BoxedIcon(
+                                WeatherIcons.strong_wind,
+                                color: Colors.white,
                               )
                             ],
                           ),
-                          Column(
-                            children: [
-                              Text(
-                                "|",
-                                style: GoogleFonts.getFont(
-                                  "Raleway",
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w300,
-                                ),
-                              )
-                            ],
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(left: 21.0, right: 23),
+                            child: Column(
+                              children: [
+                                Text(
+                                  "Wind",
+                                  style: GoogleFonts.getFont(
+                                    "Overpass",
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                  ),
+                                )
+                              ],
+                            ),
                           ),
                           Column(
                             children: [
@@ -114,25 +160,24 @@ class _HomePageState extends State<HomePage> {
                         children: [
                           Column(
                             children: [
-                              Text(
-                                "Hum",
-                                style: GoogleFonts.getFont(
-                                  "Overpass",
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                ),
+                              BoxedIcon(
+                                WeatherIcons.raindrop,
+                                color: Colors.white,
                               )
                             ],
                           ),
                           Column(
                             children: [
-                              Text(
-                                "|",
-                                style: GoogleFonts.getFont(
-                                  "Raleway",
-                                  fontSize: 18,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w300,
+                              Padding(
+                                padding: const EdgeInsets.only(
+                                    left: 21.0, right: 23),
+                                child: Text(
+                                  "Hum",
+                                  style: GoogleFonts.getFont(
+                                    "Overpass",
+                                    fontSize: 18,
+                                    color: Colors.white,
+                                  ),
                                 ),
                               )
                             ],
@@ -153,6 +198,42 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ],
                   ),
+                ),
+              ),
+            ),
+            Spacer(),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 34),
+              child: Container(
+                width: 204,
+                height: 60,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Forecast report",
+                        style: GoogleFonts.getFont(
+                          "Overpass",
+                          fontSize: 17,
+                          color: Color(0xFF444E72),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 10.0),
+                        child: Icon(
+                          Icons.navigate_next,
+                          color: Color(0xFF444E72),
+                        ),
+                      )
+                    ],
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      primary: Colors.white),
                 ),
               ),
             )
