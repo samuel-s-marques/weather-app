700d7bdde22535bbb9d4b159684ecd4d1b3e0c5d
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-12-12 20:55:25 -0300

    add hour

--- lib/widgets/card_forecast.dart
+++ lib/widgets/card_forecast.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CardForecast extends StatelessWidget {
+  final String day;
+  final String hour;
   final String temperature;
   final String asset;
-  final String day;
 
   CardForecast({
+    required this.day,
+    required this.hour,
     required this.temperature,
     required this.asset,
-    required this.day,
   });
 
   @override
@@ -17,11 +19,10 @@ class CardForecast extends StatelessWidget {
     return Card(
       color: Colors.transparent,
       elevation: 0.0,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(children: [
             Text(
               day,
               style: GoogleFonts.getFont("Overpass",
@@ -29,21 +30,28 @@ class CardForecast extends StatelessWidget {
                   fontWeight: FontWeight.bold,
                   color: Colors.white),
             ),
-            Image.asset(
-              "assets/images/$asset.png",
-              width: 35,
-            ),
             Text(
-              "$temperature°",
-              style: GoogleFonts.getFont(
-                "Overpass",
-                fontSize: 18,
-                fontWeight: FontWeight.normal,
-                color: Colors.white,
-              ),
+              hour,
+              style: GoogleFonts.getFont("Overpass",
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
             ),
-          ],
-        ),
+          ],),
+          Image.network(
+            "http://openweathermap.org/img/wn/$asset@4x.png",
+            width: 50,
+          ),
+          Text(
+            "$temperature°",
+            style: GoogleFonts.getFont(
+              "Overpass",
+              fontSize: 18,
+              fontWeight: FontWeight.normal,
+              color: Colors.white,
+            ),
+          ),
+        ],
       ),
     );
   }
