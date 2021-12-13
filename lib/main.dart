c0b4f8547ed89282fc9deaa802d4a82cb1b9ed62
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-12-12 23:22:56 -0300

    Remove unnecessary lines, add map route

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:intl/date_symbol_data_local.dart';
-import 'package:weatherapp/pages/detail_page.dart';
+import 'package:weatherapp/pages/forecast_page.dart';
 import 'package:weatherapp/pages/home_page.dart';
+import 'package:weatherapp/pages/map_page.dart';
 
 Future main() async {
   await dotenv.load(fileName: ".env");
@@ -16,13 +17,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'WeatherApp',
       home: HomePage(),
       routes: {
-        "/details": (context) => const DetailPage(),
+        "/details": (context) => const ForecastPage(),
+        "/map": (context) => const MapPage(),
       },
     );
   }
