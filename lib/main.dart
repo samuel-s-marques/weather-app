4ad73e8ec591ff9e5a1ee22a1e5849ba8f145c04
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-12-13 15:42:10 -0300

    Add text themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:weatherapp/pages/forecast_page.dart';
 import 'package:weatherapp/pages/home_page.dart';
@@ -8,7 +9,8 @@ import 'package:weatherapp/pages/map_page.dart';
 
 Future main() async {
   await dotenv.load(fileName: ".env");
-  initializeDateFormatting(Platform.localeName, null).then((_) => runApp(const MyApp()));
+  initializeDateFormatting(Platform.localeName, null)
+      .then((_) => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -18,6 +20,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'WeatherApp',
+      theme: ThemeData(
+        textTheme: TextTheme(
+          headline1: GoogleFonts.getFont("Overpass",
+              fontSize: 25,
+              fontWeight: FontWeight.w900,
+              color: Color(0xFF444E72)),
+          headline2: GoogleFonts.getFont("Overpass",
+              fontSize: 17,
+              fontWeight: FontWeight.w900,
+              color: Color(0xFF444E72)),
+        ),
+      ),
       home: HomePage(),
       routes: {
         "/details": (context) => const ForecastPage(),
