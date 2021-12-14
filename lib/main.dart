4f65f3bb5ae59892944b809035f28a9021023c66
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-12-14 10:31:01 -0300

    Remove debug mode flag

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'WeatherApp',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         textTheme: TextTheme(
           headline1: GoogleFonts.getFont("Overpass",
