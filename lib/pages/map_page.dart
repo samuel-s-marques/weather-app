f00f9756e2a48f0bdc77d43415fa2a2355bb6429
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-12-26 02:07:39 -0300

    create search history and search places

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,22 +1,27 @@
-import 'package:flutter/cupertino.dart';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:google_place/google_place.dart';
 import 'package:material_floating_search_bar/material_floating_search_bar.dart';
+import 'package:weatherapp/database/database.dart';
+import 'package:weatherapp/models/place.dart';
 import 'package:weatherapp/services/geo_api.dart';
 
 class MapPage extends StatefulWidget {
-  const MapPage({Key? key}) : super(key: key);
-
   @override
   _MapPageState createState() => _MapPageState();
 }
 
 class _MapPageState extends State<MapPage> with TickerProviderStateMixin {
-  MapController _mapController = MapController();
+  Completer<GoogleMapController> _controller = Completer();
+  FloatingSearchBarController controller = FloatingSearchBarController();
+  List<Marker> customMarkers = [];
+  List<AutocompletePrediction> _results = [];
 
   @override
   void dispose() {
@@ -28,19 +33,32 @@ class _MapPageState extends State<MapPage> with TickerProviderStateMixin {
     return Scaffold(
       body: Stack(children: [
         buildMap(),
-        buildFloatingSearchBar(),
+        FutureBuilder(
+          future: WeatherDatabase().findAll(),
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            if (snapshot.hasData) {
+              List<Place> places = snapshot.data;
+
+              return buildFloatingSearchBar(places);
+            }
+
+            return buildFloatingSearchBar([]);
+          },
+        ),
         buildFabs(),
       ]),
     );
   }
 
-  Widget buildFloatingSearchBar() {
+  Widget buildFloatingSearchBar(List<Place> places) {
     final isPortrait =
         MediaQuery.of(context).orientation == Orientation.portrait;
+    var googlePlace = GooglePlace(dotenv.get('GOOGLE_PLACE_API_KEY'));
 
     return Padding(
       padding: const EdgeInsets.only(top: 30.0),
       child: FloatingSearchBar(
+        controller: controller,
         queryStyle: GoogleFonts.getFont("Overpass",
             fontSize: 18, color: Color(0xFF444E72)),
         borderRadius: BorderRadius.circular(15),
@@ -55,7 +73,19 @@ class _MapPageState extends State<MapPage> with TickerProviderStateMixin {
         openAxisAlignment: 0.0,
         width: isPortrait ? 600 : 500,
         debounceDelay: const Duration(milliseconds: 500),
-        onQueryChanged: (query) {},
+        onQueryChanged: (query) async {
+          var places = await googlePlace.autocomplete.get(query);
+          _results = places!.predictions!;
+
+          for (var result in _results) {
+            WeatherDatabase().save(Place(
+              placeId: result.placeId ?? '',
+              placeDescription: result.description ?? '',
+            ));
+          }
+
+          setState(() {});
+        },
         transition: CircularFloatingSearchBarTransition(),
         actions: [
           FloatingSearchBarAction(
@@ -75,14 +105,34 @@ class _MapPageState extends State<MapPage> with TickerProviderStateMixin {
             child: Material(
               color: Colors.white,
               elevation: 4.0,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: Colors.accents.map((color) {
-                  return Container(
-                    height: 112,
-                    color: color,
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount:
+                    _results.length > 0 ? _results.length : places.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    leading: _results.isNotEmpty
+                        ? const Icon(Icons.place_outlined)
+                        : const Icon(Icons.history_outlined),
+                    onTap: () async {
+                      var placeDetails = await googlePlace.details.get(
+                          _results.isNotEmpty
+                              ? _results[index].placeId ?? ''
+                              : places[index].placeId);
+
+                      goToLocation(placeDetails!.result!.geometry!.location!.lat, placeDetails.result!.geometry!.location!.lng);
+                      controller.close();
+                    },
+                    title: Text(
+                      "${_results.isNotEmpty ? _results[index].description : places[index].placeDescription}",
+                      style: GoogleFonts.getFont(
+                        "Overpass",
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   );
-                }).toList(),
+                },
               ),
             ),
           );
@@ -92,48 +142,49 @@ class _MapPageState extends State<MapPage> with TickerProviderStateMixin {
   }
 
   Widget buildMap() {
-    return FlutterMap(
-      mapController: _mapController,
-      options: MapOptions(
-        center: LatLng(51.5, -0.09),
-        zoom: 13.0,
-      ),
-      layers: [
-        TileLayerOptions(
-          urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-          subdomains: ['a', 'b', 'c'],
-        ),
-      ],
+    final CameraPosition _kGooglePlex = CameraPosition(
+      target: LatLng(37.42796133580664, -122.085749655962),
+      zoom: 14.4746,
+    );
+
+    return GoogleMap(
+      initialCameraPosition: _kGooglePlex,
+      mapType: MapType.normal,
+      zoomControlsEnabled: false,
+      myLocationEnabled: true,
+      myLocationButtonEnabled: false,
+      markers: customMarkers.toSet(),
+      onMapCreated: (GoogleMapController controller) {
+        _controller.complete(controller);
+      },
     );
   }
 
-  void _animatedMapMove(LatLng destLocation, double destZoom) {
-    final _latTween = Tween<double>(
-        begin: _mapController.center.latitude, end: destLocation.latitude);
-    final _lngTween = Tween<double>(
-        begin: _mapController.center.longitude, end: destLocation.longitude);
-    final _zoomTween = Tween<double>(begin: _mapController.zoom, end: destZoom);
+  Future<void> _goToMyPosition() async {
+    Position _currentPosition = await GeoApi().determinePosition();
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          bearing: 0,
+          zoom: 16,
+          target: LatLng(_currentPosition.latitude, _currentPosition.longitude),
+        ),
+      ),
+    );
+  }
 
-    var controller = AnimationController(
-        vsync: this, duration: const Duration(milliseconds: 500));
-    Animation<double> animation =
-        CurvedAnimation(parent: controller, curve: Curves.fastOutSlowIn);
-
-    controller.addListener(() {
-      _mapController.move(
-          LatLng(_latTween.evaluate(animation), _lngTween.evaluate(animation)),
-          _zoomTween.evaluate(animation));
-    });
-
-    animation.addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        controller.dispose();
-      } else if (status == AnimationStatus.dismissed) {
-        controller.dispose();
-      }
-    });
-
-    controller.forward();
+  Future<void> goToLocation(latitude, longitude) async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(
+      CameraUpdate.newCameraPosition(
+        CameraPosition(
+          bearing: 0,
+          zoom: 16,
+          target: LatLng(latitude, longitude),
+        ),
+      ),
+    );
   }
 
   Widget buildFabs() {
@@ -145,14 +196,7 @@ class _MapPageState extends State<MapPage> with TickerProviderStateMixin {
           mainAxisSize: MainAxisSize.min,
           children: [
             FloatingActionButton(
-              onPressed: () async {
-                Position _currentPosition = await GeoApi().determinePosition();
-
-                _animatedMapMove(
-                    LatLng(
-                        _currentPosition.latitude, _currentPosition.longitude),
-                    13.0);
-              },
+              onPressed: _goToMyPosition,
               backgroundColor: Colors.white,
               child: Icon(Icons.gps_fixed, color: Color(0xFF4D4D4D)),
             ),
