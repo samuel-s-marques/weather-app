5c4c18cfd1b5b9e14c5257af400e5babef41292f
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-12-28 16:03:33 -0300

    change from portuguese language to the current one

--- lib/services/weather_api.dart
+++ lib/services/weather_api.dart
@@ -1,14 +1,22 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:weather/weather.dart';
+import 'package:weatherapp/utils/utils.dart';
 
 class WeatherApi {
-  final WeatherFactory _wf = WeatherFactory(dotenv.get("API_KEY"), language: Language.PORTUGUESE_BRAZIL);
+  Future<Weather> getCurrentWeather(String cityName, BuildContext context) async {
+    Locale currentLocale = Localizations.localeOf(context);
+    String currentLanguageCode = currentLocale.languageCode;
+    WeatherFactory _wf = WeatherFactory(dotenv.get("API_KEY"), language: languageFromCode(currentLanguageCode));
 
-  Future<Weather> getCurrentWeather(String cityName) async {
     return await _wf.currentWeatherByCityName(cityName);
   }
 
-  Future<List<Weather>> getNextFiveDaysForecast(String cityName) async {
+  Future<List<Weather>> getNextFiveDaysForecast(String cityName, BuildContext context) async {
+    Locale currentLocale = Localizations.localeOf(context);
+    String currentLanguageCode = currentLocale.languageCode;
+    WeatherFactory _wf = WeatherFactory(dotenv.get("API_KEY"), language: languageFromCode(currentLanguageCode));
+
     return await _wf.fiveDayForecastByCityName(cityName);
   }
 }
