3bbc533958f7608c758e4e7c938465b27ed4b1a8
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-12-28 15:56:57 -0300

    Add favoriteplace

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -13,12 +13,18 @@ class WeatherDatabase {
               'placeId TEXT,'
               'placeDescription TEXT)',
         );
+        db.execute(
+          'CREATE TABLE favoritePlaces('
+              'id INTEGER PRIMARY KEY,'
+              'placeId TEXT,'
+              'placeName TEXT)',
+        );
       }, version: 1);
     });
   }
 
-  Future<int> save(Place place) async {
-    int count = await getCount();
+  Future<int> saveRecentSearch(Place place) async {
+    int count = await getCount("recentSearches");
 
     return createDatabase().then((db) async {
       final Map<String, String> searchMap = {};
@@ -27,7 +33,7 @@ class WeatherDatabase {
       searchMap['placeDescription'] = place.placeDescription;
 
       if (count >= 5) {
-        await deleteAll();
+        await deleteAll('recentSearches');
         return await db.insert('recentSearches', searchMap);
       }
 
@@ -35,7 +41,25 @@ class WeatherDatabase {
     });
   }
 
-  Future<List<Place>> findAll() {
+  Future<int> saveFavoritePlace(Place place) async {
+    int count = await getCount("favoritePlaces");
+
+    return createDatabase().then((db) async {
+      final Map<String, String> searchMap = {};
+
+      searchMap['placeId'] = place.placeId;
+      searchMap['placeName'] = place.placeDescription;
+
+      if (count >= 5) {
+        await deleteAll('favoritePlaces');
+        return await db.insert('favoritePlaces', searchMap);
+      }
+
+      return await db.insert('favoritePlaces', searchMap);
+    });
+  }
+
+  Future<List<Place>> findAllRecentSearches() {
     return createDatabase().then((db) {
       return db.query('recentSearches').then((maps) {
         final List<Place> places = [];
@@ -54,18 +78,43 @@ class WeatherDatabase {
     });
   }
 
-  Future<int> getCount() async {
+  Future<List<Place>> findAllFavoritePlaces() {
+    return createDatabase().then((db) {
+      return db.query('favoritePlaces').then((maps) {
+        final List<Place> places = [];
+
+        for (Map<String, dynamic> map in maps) {
+          final Place place = Place(
+            placeId: map['placeId'],
+            placeDescription: map['placeName'],
+          );
+
+          places.add(place);
+        }
+
+        return places;
+      });
+    });
+  }
+
+  Future<int> getCount(String table) async {
     return createDatabase().then((db) async {
-      var x = await db.rawQuery("SELECT COUNT (*) FROM recentSearches");
+      var x = await db.rawQuery("SELECT COUNT (*) FROM $table");
 
       int count = Sqflite.firstIntValue(x) ?? 0;
       return count;
     });
   }
 
-  Future<int> deleteAll() async {
+  Future<int> deleteAll(String table) async {
     return createDatabase().then((db) async {
-      return await db.rawDelete("DELETE FROM recentSearches");
+      return await db.rawDelete("DELETE FROM $table");
+    });
+  }
+
+  Future<int> deleteWhere(String placeId, String table) async {
+    return createDatabase().then((db) async {
+      return await db.delete(table, where: 'placeId = ?', whereArgs: [placeId]);
     });
   }
 }
