2728a482f56e775b8dfc07219b4c5ed1fce90acc
Samuel Marques <73559465+samuel-s-marques@users.noreply.github.com>
2021-12-28 16:03:46 -0300

    Add languageFromCode

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,3 +1,5 @@
+import 'package:weather/weather.dart';
+
 extension StringExtension on String {
   String capitalize() {
     if (this.isEmpty || this == null) return this;
@@ -6,3 +8,60 @@ extension StringExtension on String {
   }
 }
 
+Map<Language, String> _languageCode = {
+  Language.AFRIKAANS: 'af',
+  Language.ALBANIAN: 'al',
+  Language.ARABIC: 'ar',
+  Language.AZERBAIJANI: 'az',
+  Language.BULGARIAN: 'bg',
+  Language.CATALAN: 'ca',
+  Language.CZECH: 'cz',
+  Language.DANISH: 'da',
+  Language.GERMAN: 'de',
+  Language.GREEK: 'el',
+  Language.ENGLISH: 'en',
+  Language.BASQUE: 'eu',
+  Language.PERSIAN: 'fa',
+  Language.FARSI: 'fa',
+  Language.FINNISH: 'fi',
+  Language.FRENCH: 'fr',
+  Language.GALICIAN: 'gl',
+  Language.HEBREW: 'he',
+  Language.HINDI: 'hi',
+  Language.CROATIAN: 'hr',
+  Language.HUNGARIAN: 'hu',
+  Language.INDONESIAN: 'id',
+  Language.ITALIAN: 'it',
+  Language.JAPANESE: 'ja',
+  Language.KOREAN: 'kr',
+  Language.LATVIAN: 'la',
+  Language.LITHUANIAN: 'lt',
+  Language.MACEDONIAN: 'mk',
+  Language.NORWEGIAN: 'no',
+  Language.DUTCH: 'nl',
+  Language.POLISH: 'pl',
+  Language.PORTUGUESE: 'pt',
+  Language.PORTUGUESE_BRAZIL: 'pt_br',
+  Language.ROMANIAN: 'ro',
+  Language.RUSSIAN: 'ru',
+  Language.SWEDISH: 'sv',
+  Language.SLOVAK: 'sk',
+  Language.SLOVENIAN: 'sl',
+  Language.SPANISH: 'sp',
+  Language.SERBIAN: 'sr',
+  Language.THAI: 'th',
+  Language.TURKISH: 'tr',
+  Language.UKRAINIAN: 'ua',
+  Language.VIETNAMESE: 'vi',
+  Language.CHINESE_SIMPLIFIED: 'zh_cn',
+  Language.CHINESE_TRADITIONAL: 'zh_tw',
+  Language.ZULU: 'zu',
+};
+
+Language languageFromCode(String code) {
+  for (var entry in _languageCode.entries) {
+    if (entry.value == code) return entry.key;
+  }
+
+  return Language.ENGLISH;
+}
